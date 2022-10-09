4f7d3c63c2ad45f8124421ae95544c40c329c466
liangkang <liangkang1436@163.com>
2022-10-09 15:40:59 +0800

    优化注释

--- SpringBoot-Basic/src/main/java/xyz/xiashuo/controller/HelloController.java
+++ SpringBoot-Basic/src/main/java/xyz/xiashuo/controller/HelloController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/Hello")
 public class HelloController {
 
     // http://localhost:8888/SpringBoot-Basic/hello
-    @RequestMapping("/hello")
-    public String handle01(){
+    @RequestMapping("/sayHello")
+    public String sayHello() {
         return "Hello, Spring Boot 2!";
     }
-
 
 }
