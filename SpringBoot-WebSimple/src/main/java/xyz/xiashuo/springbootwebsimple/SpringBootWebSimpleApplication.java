931f32b25cf80d61d7711e306586da310fa6de1f
liangkang <liangkang1436@163.com>
2022-11-03 01:09:16 +0800

    静态资源处理

---
+++ SpringBoot-WebSimple/src/main/java/xyz/xiashuo/springbootwebsimple/SpringBootWebSimpleApplication.java
@@ -0,0 +1,13 @@
+package xyz.xiashuo.springbootwebsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebSimpleApplication.class, args);
+    }
+
+}
---
+++ SpringBoot-WebSimple/src/test/java/xyz/xiashuo/springbootwebsimple/SpringBootWebSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xiashuo.springbootwebsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
