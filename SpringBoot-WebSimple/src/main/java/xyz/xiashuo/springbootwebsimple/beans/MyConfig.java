78320b75a0eb15a8ca4209fa7d843c797fd3b18d
liangkang <liangkang1436@163.com>
2022-11-10 15:04:18 +0800

    SpringBoot Web半自动配置

---
+++ SpringBoot-WebSimple/src/main/java/xyz/xiashuo/springbootwebsimple/beans/MyConfig.java
@@ -0,0 +1,30 @@
+package xyz.xiashuo.springbootwebsimple.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+
+/**
+ * 使用SpringBoot自动配置，同时自己自定义Web
+ */
+@Configuration(proxyBeanMethods = false)
+public class MyConfig implements WebMvcConfigurer {
+
+    @Bean
+    public HiddenHttpMethodFilter myHiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+        filter.setMethodParam("_m");
+        return filter;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper pathHelper = new UrlPathHelper();
+        pathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(pathHelper);
+    }
+
+}
